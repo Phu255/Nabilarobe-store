e3da70fb269295183121e4c66e130772de44c160
Phu <phuhuy255@gmail.com>
2022-08-03 16:15:57 +0700

    sort file by name before upload it will sort them ,add more color to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
     has_one_attached :avatar
     has_many_attached :images
     has_many_attached :images2
-    validates :title, :description, :size ,:price ,:presence => true
+    validates :title, :description, :size ,:price ,:avatar ,:images,:presence => true
     validates :price, :numericality => {:greater_than_or_equal_to => 100000}
     validates :title,:uniqueness => true
     
